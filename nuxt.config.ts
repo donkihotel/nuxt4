005793fb807791072651732ea8461a6cdf92a108
donkihotel <106761177+donkihotel@users.noreply.github.com>
2026-04-10 12:34:43 +0900

    update 7

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,6 +39,19 @@ export default defineNuxtConfig({
           async: true,
           'data-key': 'KZQYFtkPbslgyuy51XUE7A'
         }
+      ],
+      meta: [
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: '2CPU2RAM' },
+        { property: 'og:title', content: '2CPU2RAM' },
+        { property: 'og:description', content: '서버 구축부터 도메인 연결, 메일 설정까지 한 번에—홈페이지를 쉽고 빠르게 운영할 수 있도록 도와드립니다.' },
+        { property: 'og:url', content: 'https://2cpu2ram.com' },
+        { property: 'og:image', content: 'https://2cpu2ram.com/og-image.png' },
+
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: '2CPU2RAM' },
+        { name: 'twitter:description', content: '서버 구축부터 도메인 연결, 메일 설정까지 한 번에—홈페이지를 쉽고 빠르게 운영할 수 있도록 도와드립니다.' },
+        { name: 'twitter:image', content: 'https://2cpu2ram.com/og-image.png' }
       ]
     }
   },
@@ -53,10 +66,10 @@ export default defineNuxtConfig({
   // pinia: {
   //   autoImports: ['defineStore', 'acceptHMRUpdate']
   // },
-  site: {
-    url: 'https://2cpu2ram.com',
-    name: '2CPU2RAM'
-  },
+  // site: {
+  //   url: 'https://2cpu2ram.com',
+  //   name: '2CPU2RAM'
+  // },
   // sitemap: {
   //   exclude: ['/pinia/**', '/fetch/**'],
   // }
